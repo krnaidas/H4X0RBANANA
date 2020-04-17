29aba6d3f58a54775db35506a32abc0f58071aea
Karlos Naidas <krnaidas@gmail.com>
2020-04-17 16:15:52 +0800

    Initial Commit

---
+++ H4X0RNAIDAS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0RNAIDAS
+//
+//  Created by Naidas on 4/17/20.
+//  Copyright © 2020 Naidas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
